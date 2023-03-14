2fdd91f644fda773c2fa5c11b60d8b2628dcf39e
Greg Quigg <grquigg@email.wm.edu>
2023-03-14 14:15:30 -0400

    Got run round working again

--- matches.py
+++ matches.py
@@ -7,12 +7,13 @@ class Match():
         self.teamA = teamA
         self.teamB = teamB
         self.channel = channel
-        self.flavorText = flavorText
+        self.gen = flavorText
         self.teamData = teamData
         self.team_rosters = team_rosters
+        self.gen = FlavorTextGenerator(self.gen)
 
     async def beater_turn(self, beater, channel, a, b):
-        response = await self.flavorText.generateNextBeaterText("crit")
+        response = await self.gen.generateNextBeaterText("crit")
         roll = random.randint(1, 12) + beater["rank"]
         if (beater["injured"]):
             roll =- 1
@@ -30,7 +31,7 @@ class Match():
                     hit_player = int
             hit = list(self.team_rosters[a].items())[hit_player]
             if(channel != None):
-                await channel.send("{b} has mistakenly directed the bludger at their own teammate, {h}".format(b=beater["name"], h=teamData[hit[1]]["name"]))
+                await channel.send("{b} has mistakenly directed the bludger at their own teammate, {h}".format(b=beater["name"], h=self.teamData[hit[1]]["name"]))
                 if(self.teamData[hit[1]]["injured"] == True):
                     print("Player was already injured")
                     response = "{b} has knocked {h} out of the game! {a} will have to substitute"
@@ -59,13 +60,13 @@ class Match():
             text = ""
             action = ""
             if("Chaser" in foul[0]):
-                text = await self.generateNextBeaterText("fouls", position="chaser")
-                action = await self.generateNextBeaterText("fouls", position="moves")
+                text = await self.gen.generateNextBeaterText("fouls", position="chaser")
+                action = await self.gen.generateNextBeaterText("fouls", position="moves")
             elif("Keeper" in foul[0]):
-                text = await self.generateNextBeaterText("fouls", position="keeper")
+                text = await self.gen.generateNextBeaterText("fouls", position="keeper")
             elif("Beater" in foul[0]):
-                text = await self.generateNextBeaterText("fouls")
-                action = await self.generateNextBeaterText("fouls", position="moves")
+                text = await self.gen.generateNextBeaterText("fouls")
+                action = await self.gen.generateNextBeaterText("fouls", position="moves")
             print(text)
             await channel.send(text.format(name=foul[1], house=a, action=action, number=self.teamData[foul[1]]["jersey"]))
             scores[a] -= 10
@@ -73,7 +74,7 @@ class Match():
             # raise NotImplementedError("fix local variable 'action' referenced before assignment error")
         elif(roll>= 4 and roll < 8):
             #generate text
-            text = await generateNextBeaterText("miss")
+            text = await self.gen.generateNextBeaterText("miss")
             print(text)
             min_roll = random.randint(1, 12)
             hit_player = 0
@@ -82,11 +83,11 @@ class Match():
                 if(min_roll > comparison_roll):
                     min_roll = comparison_roll
                     hit_player = int
-            hit = list(team_rosters[b].items())[hit_player]
+            hit = list(self.team_rosters[b].items())[hit_player]
             if(channel != None):
-                await channel.send(text.format(beater=beater["name"], opp_name=teamData[hit[1]]["name"], house=b, num=teamData[hit[1]]["jersey"]))
+                await channel.send(text.format(beater=beater["name"], opp_name=self.teamData[hit[1]]["name"], house=b, num=self.teamData[hit[1]]["jersey"]))
         elif(roll >= 8 or roll < 12):
-            text = await generateNextBeaterText("minor")
+            text = await self.gen.generateNextBeaterText("minor")
             print(text)
             min_roll = random.randint(1, 12)
             hit_player = 0
@@ -95,25 +96,25 @@ class Match():
                 if(min_roll > comparison_roll):
                     min_roll = comparison_roll
                     hit_player = int
-            hit = list(team_rosters[b].items())[hit_player]
-            if(teamData[hit[1]]["injured"] == True):
+            hit = list(self.team_rosters[b].items())[hit_player]
+            if(self.teamData[hit[1]]["injured"] == True):
                 if(channel != None):
-                    await channel.send(beater["name"] + " hit " + str(teamData[hit[1]]["name"]) + " when they were they were already injured!")
-                    await channel.send(str(teamData[hit[1]]["name"]) + " was knocked unconscious and cannot play")
+                    await channel.send(beater["name"] + " hit " + str(self.teamData[hit[1]]["name"]) + " when they were they were already injured!")
+                    await channel.send(str(self.teamData[hit[1]]["name"]) + " was knocked unconscious and cannot play")
                     beater["xp"] += 100
-                teamData[hit[1]]["critically_injured"] = True
+                self.teamData[hit[1]]["critically_injured"] = True
                 if(channel != None):
                     await channel.send("Need to substitute for the position " + str(hit[0]))
-                team_rosters[b][hit[0]] = None
-                while(team_rosters[b][hit[0]] == None):
+                self.team_rosters[b][hit[0]] = None
+                while(self.team_rosters[b][hit[0]] == None):
                     if(channel == None or b != "Gryffindor"):
                         print("Call search for sub")
                         await search_for_sub(b, hit)
                     await asyncio.sleep(1)
             else:
-                teamData[hit[1]]["injured"] = True
+                self.teamData[hit[1]]["injured"] = True
                 if(channel != None):
-                    await channel.send(text.format(beater=beater["name"], opp_name=teamData[hit[1]]["name"], opp_pos=teamData[hit[1]]["position"]))
+                    await channel.send(text.format(beater=beater["name"], opp_name=self.teamData[hit[1]]["name"], opp_pos=teamData[hit[1]]["position"]))
                     await channel.send("{opp_name} is now injured and will roll with disadvantage".format(opp_name=teamData[hit[1]]["name"]))
 
             beater["xp"] += 50
@@ -126,15 +127,15 @@ class Match():
                 if(min_roll > comparison_roll):
                     min_roll = comparison_roll
                     hit_player = int
-            hit = list(team_rosters[b].items())[hit_player]
+            hit = list(self.team_rosters[b].items())[hit_player]
             if(channel != None):
-                await channel.send(beater["name"] + " hit " + str(teamData[hit[1]]["name"]) + " and landed a critical hit!")
+                await channel.send(beater["name"] + " hit " + str(self.teamData[hit[1]]["name"]) + " and landed a critical hit!")
             beater["xp"] += 100
             await check_for_level_up(beater, channel)
-            team_rosters[b][hit[0]] = None
+            self.team_rosters[b][hit[0]] = None
             if (channel != None):
                 await channel.send("Need to substitute for the position " + str(hit[0]))
-            while(team_rosters[b][hit[0]] == None):
+            while(self.team_rosters[b][hit[0]] == None):
                 if(channel != None or b != "Gryffindor"):
                     await search_for_sub(b, hit)
                 await asyncio.sleep(1)
@@ -207,7 +208,7 @@ class Match():
             else:
                 continue
 
-    async def run_round(a, b, channel, team_rosters, teamData, flavorGen, init=False):
+    async def run_round(self, a, b, init=False):
         # global control_sequence
         # global team_A_rolls
         # global team_B_rolls
@@ -238,22 +239,18 @@ class Match():
 
         for k in range(maxRoll):
             r = ()
-            if(k % 3 == 0):
-                chaser = team_rosters[a]["Chaser1"]
-            elif(k % 3 == 1):
-                chaser = team_rosters[a]["Chaser2"]
-            else:
-                chaser = team_rosters[a]["Chaser3"]
-            r = (chaser, (random.randint(1, 12) + teamData[chaser]["rank"]))
-            if(teamData[chaser]["injured"]):
+            val = (k % 3) + 1
+            chaser = self.team_rosters[a]["Chaser{}".format(val)]
+            r = (chaser, (random.randint(1, 12) + self.teamData[chaser]["rank"]))
+            if(self.teamData[chaser]["injured"]):
                 roll = r[1]
                 r = (chaser, roll - 1)
 
             team_A_rolls.append(r)
-            keeper = team_rosters[b]["Keeper"]
-            team_B_rolls.append(random.randint(1, 12) + teamData[keeper]["rank"])
+            keeper = self.team_rosters[b]["Keeper"]
+            team_B_rolls.append(random.randint(1, 12) + self.teamData[keeper]["rank"])
 
-            if(teamData[keeper]["injured"]):
+            if(self.teamData[keeper]["injured"]):
                 team_B_rolls[k] -= 1
                 
         team_A_rolls.sort(reverse=True, key = lambda x: x[1])
@@ -261,7 +258,7 @@ class Match():
         #have to keep track of the index
         if(init):
             print("init")
-            await channel.send("{chaser} has caught the Quaffle! {house} starts the game".format(chaser = teamData[team_A_rolls[0][0]]["name"], house=a))
+            await self.channel.send("{chaser} has caught the Quaffle! {house} starts the game".format(chaser = self.teamData[team_A_rolls[0][0]]["name"], house=a))
             quaffle_possession = team_A_rolls[0][0]
             prev_possession = team_A_rolls[0][0]
         else:
@@ -270,89 +267,88 @@ class Match():
         misses = 0
         for x in range(maxRoll):
             index_i = x
-            offensiveMove = await flavorGen.generateNextMove("offensive")
+            offensiveMove = await self.gen.generateNextMove("offensive")
             if(beater_rolls[0] == x):
                 print("Beater 1 turn")
-                beater = teamData[team_rosters[a]["Beater1"]]
-                await beater_turn(beater, channel, a, b)
+                beater = self.teamData[self.team_rosters[a]["Beater1"]]
+                await self.beater_turn(beater, self.channel, a, b)
 
             if(beater_rolls[1] == x):
                 print("Beater 2 turn")
-                beater = teamData[team_rosters[a]["Beater2"]]
-                await beater_turn(beater, channel, a, b)
+                beater = self.teamData[self.team_rosters[a]["Beater2"]]
+                await self.beater_turn(beater, self.channel, a, b)
 
             if (team_A_rolls[x][1] > team_B_rolls[x]):
                 print("success")
-                teamData[team_A_rolls[x][0]]["xp"] += 25
-                keeper = team_rosters[b]["Keeper"]
+                self.teamData[team_A_rolls[x][0]]["xp"] += 25
+                keeper = self.team_rosters[b]["Keeper"]
                 if(quaffle_possession == team_A_rolls[x][0]): #same
-                    same = await flavorGen.generateNextText("same")
+                    same = await self.gen.generateNextText("same")
                     print(same)
-                    counter = await flavorGen.generateNextMove("counter")
+                    counter = await self.gen.generateNextMove("counter")
                     #there should be a distinct move name for this
                     #also if this happens more than once, then stop it
                     #there should be a roll for "interception"
-                    await channel.send("{chaser} {action} {opposing} with a {counter} and scores a goal using a {offensive} to get past {keeper}".format(chaser=teamData[team_A_rolls[x][0]]["name"], 
-                    action=same, opposing=b, counter=counter, offensive=offensiveMove, keeper=teamData[keeper]["name"]))
+                    await self.channel.send("{chaser} {action} {opposing} with a {counter} and scores a goal using a {offensive} to get past {keeper}".format(chaser=self.teamData[team_A_rolls[x][0]]["name"], 
+                    action=same, opposing=b, counter=counter, offensive=offensiveMove, keeper=self.teamData[keeper]["name"]))
                     
                 else:
-                    different = await flavorGen.generateNextText("different")
-                    counter = await flavorGen.generateNextMove("counter")
+                    different = await self.gen.generateNextText("different")
+                    counter = await self.gen.generateNextMove("counter")
                     string = "{q} {dif} with a {counter}, passing the Quaffle to {r} who scores a goal with a {offensive}"
-                    await channel.send(string.format(q=teamData[quaffle_possession]["name"], dif=different, counter=counter, r=teamData[team_A_rolls[x][0]]["name"], offensive=offensiveMove))
-                await check_for_level_up(teamData[team_A_rolls[x][0]], channel)
+                    await self.channel.send(string.format(q=self.teamData[quaffle_possession]["name"], dif=different, counter=counter, r=self.teamData[team_A_rolls[x][0]]["name"], offensive=offensiveMove))
+                await check_for_level_up(self.teamData[team_A_rolls[x][0]], self.channel)
                 scores[team_a] += 30
                 successes += 1
                 quaffle_possession = team_A_rolls[x][0]
             elif(team_A_rolls[x][1] < team_B_rolls[x]):
                 print("miss")
-                teamData[team_A_rolls[x][0]]["xp"] += 5
-                keeper = team_rosters[b]["Keeper"]
+                self.teamData[team_A_rolls[x][0]]["xp"] += 5
+                keeper = self.team_rosters[b]["Keeper"]
                 textType = "non-final"
                 if(x == maxRoll-1):
                     textType = "final"
-                response = await flavorGen.generateNextMissText(textType)
+                response = await self.gen.generateNextMissText(textType)
                 print(response)
-                teamData[keeper]["xp"] += 10
+                self.teamData[keeper]["xp"] += 10
                 misses += 1
                 string = "{keeper} {response} {chaser}'s {offensive}. Possession returns to {other}."
-                await channel.send(string.format(keeper = teamData[keeper]["name"], response = response, chaser = teamData[team_A_rolls[x][0]]["name"], offensive = offensiveMove, other=a))
-                await check_for_level_up(teamData[team_A_rolls[x][0]], channel)
-                await check_for_level_up(teamData[keeper], channel)
+                await self.channel.send(string.format(keeper = self.teamData[keeper]["name"], response = response, chaser = self.teamData[team_A_rolls[x][0]]["name"], offensive = offensiveMove, other=a))
+                await check_for_level_up(self.teamData[team_A_rolls[x][0]], self.channel)
+                await check_for_level_up(self.teamData[keeper], self.channel)
             else:
                 continue
-            await channel.send("Type 'continue' to continue")
+            await self.channel.send("Type 'continue' to continue")
             while(control_sequence == False):
                 await asyncio.sleep(1)
             control_sequence = False
-        finalMove = await flavorGen.generateNextMissText("final")
-        flavor = await flavorGen.generateNextMissText("sf")
+        finalMove = await self.gen.generateNextMissText("final")
+        flavor = await self.gen.generateNextMissText("sf")
         string = "{keeper} catches {quaffle}'s attack, {final} the {off} {f}"
-        await channel.send(string.format(keeper = teamData[team_rosters[b]["Keeper"]]["name"], quaffle = teamData[quaffle_possession]["name"], final=finalMove, off=a, f=flavor))
+        await self.channel.send(string.format(keeper = self.teamData[self.team_rosters[b]["Keeper"]]["name"], quaffle = self.teamData[quaffle_possession]["name"], final=finalMove, off=a, f=flavor))
         score = "{teamA} scores {success} goals and {teamB} blocks {miss} goals. The total score thus far is {teamA}: {scoreA}, {teamB}: {scoreB}"
-        await channel.send(score.format(teamA=a, success=successes, teamB = b, miss = misses, scoreA = scores[team_a], scoreB = scores[team_b]))
+        await self.channel.send(score.format(teamA=a, success=successes, teamB = b, miss = misses, scoreA = scores[team_a], scoreB = scores[team_b]))
 
-    async def start_match(channel, teamA, teamB, team_rosters, teamData, flavorText):
-        gen = FlavorTextGenerator(flavorText)
+    async def start_match(self):
         A = random.randint(1, 20)
         B = random.randint(1, 20)
         num_rounds = random.randint(1, 12)
         scores = {}
-        scores[teamA] = 0
-        scores[teamB] = 0
+        scores[self.teamA] = 0
+        scores[self.teamB] = 0
         first_team = ""
         second_team = ""
         coin = random.randint(0, 1)
         if(coin == 0):
-            await channel.send(teamA + " will be on the offensive first")
-            first_team = teamA
-            second_team = teamB
+            await self.channel.send(self.teamA + " will be on the offensive first")
+            first_team = self.teamA
+            second_team = self.teamB
         else:
-            await channel.send(teamB + " will be on the offensive first")
-            first_team = teamB
-            second_team = teamA
-        await run_round(first_team, second_team, channel, team_rosters, teamData, gen, init=True)
-        await channel.send("Type 'continue' to continue")
+            await self.channel.send(self.teamB + " will be on the offensive first")
+            first_team = self.teamB
+            second_team = self.teamA
+        await self.run_round(first_team, second_team, init=True)
+        await self.channel.send("Type 'continue' to continue")
         while(control_sequence == False):
             await asyncio.sleep(1)
         await channel.send("Possession has turned over to " + str(second_team))
